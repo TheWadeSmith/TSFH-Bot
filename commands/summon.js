1ef158ca5c0d00a56191771fb6c78f0b11f48f89
Wade <wadesmith00@gmail.com>
2021-05-28 15:09:24 -0400

    6

--- commands/summon.js
+++ commands/summon.js
@@ -3,5 +3,19 @@ module.exports = {
     description: "this is a ping command!",
     execute(message, args){
         message.channel.send('Now playing!');
+        client.on('message', async message => {
+            // Voice only works in guilds, if the message does not come from a guild,
+            // we ignore it
+            if (!message.guild) return;
+          
+            if (message.content === '/join') {
+              // Only try to join the sender's voice channel if they are in one themselves
+              if (message.member.voice.channel) {
+                const connection = await message.member.voice.channel.join();
+              } else {
+                message.reply('You need to join a voice channel first!');
+              }
+            }
+          });
     }
 }
--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const ytdl = require('ytdl-core');
 
 const prefix = 'tsfh!';
 
 const fs = require('fs');
-
-
 
 client.commands = new Discord.Collection();
 
@@ -22,21 +19,20 @@ client.once('ready', () => {
     console.log('Two Steps From Hell Radio is now online!');
 });
 
-client.on('message', async message => {
-    // Voice only works in guilds, if the message does not come from a guild,
-    // we ignore it
-    if (!message.guild) return;
-  
-    if (message.content === prefix + 'join') {
-      // Only try to join the sender's voice channel if they are in one themselves
-      if (message.member.voice.channel) {
-        const connection = await message.member.voice.channel.join();
-        connection.play(ytdl('https://www.youtube.com/watch?v=AfMV4ZC4AnA', { quality: 'highestaudio' }));
-      } else {
-        message.reply('You need to join a voice channel first!');
-      }
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'sendamessage'){
+        client.commands.get('sendamessage').execute(message, args);
+    } 
+     if (command === 'summon')
+    {
+        client.commands.get('summon').execute(message, args);
     }
-  });
+});
 
 
 
